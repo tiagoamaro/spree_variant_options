9c7d6567216719fce8050d47237af3019282860f
Miguel Sancho <sanchojaf@gmail.com>
2014-05-20 20:42:48 -0400

    fixed issues with wrong path to copy js and css

--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -9,14 +9,16 @@ module SpreeVariantOptions
       end
 
       def add_javascripts
-        append_file "vendor/assets/javascripts/store/all.js", "//= require spree_variant_options\n"
+        append_file "vendor/assets/javascripts/spree/frontend/all.js", "//= require spree_variant_options\n"
       end
 
       def add_stylesheets
-        inject_into_file "vendor/assets/stylesheets/store/all.css", "*= require store/variant_options\n", :before => /\*\//, :verbose => true
-        inject_into_file "vendor/assets/stylesheets/admin/all.css", "*= require admin/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/frontend/all.css", "*= require store/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/spree/backend/all.css", "*= require admin/variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
   end
 end
+
+   

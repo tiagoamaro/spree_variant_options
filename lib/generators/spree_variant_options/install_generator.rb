f0b445d06fa5d35253537a249883b84a003aef89
Miguel Sancho <sanchojaf@gmail.com>
2014-05-20 20:38:28 -0400

    chaged the generator to copy files to vendor

--- lib/generators/spree_variant_options/install_generator.rb
+++ lib/generators/spree_variant_options/install_generator.rb
@@ -9,12 +9,12 @@ module SpreeVariantOptions
       end
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require spree_variant_options\n"
+        append_file "vendor/assets/javascripts/store/all.js", "//= require spree_variant_options\n"
       end
 
       def add_stylesheets
-        inject_into_file "app/assets/stylesheets/store/all.css", "*= require store/variant_options\n", :before => /\*\//, :verbose => true
-        inject_into_file "app/assets/stylesheets/admin/all.css", "*= require admin/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/store/all.css", "*= require store/variant_options\n", :before => /\*\//, :verbose => true
+        inject_into_file "vendor/assets/stylesheets/admin/all.css", "*= require admin/variant_options\n", :before => /\*\//, :verbose => true
       end
 
     end
